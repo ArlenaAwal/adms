e212fc452c86730a3be61a9a87a4845f74507f23
Arlena Awal <arlenawalchaity@gmail.com>
2022-03-27 01:11:45 +0600

    adms

---
+++ registrationadms/registrationadms/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace registrationadms
+{
+    partial class name
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textname = new System.Windows.Forms.TextBox();
+            this.textpass = new System.Windows.Forms.TextBox();
+            this.textuname = new System.Windows.Forms.TextBox();
+            this.textphn = new System.Windows.Forms.TextBox();
+            this.textadd = new System.Windows.Forms.TextBox();
+            this.textconpass = new System.Windows.Forms.TextBox();
+            this.chkboxShowPass = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(356, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Started";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label2.Location = new System.Drawing.Point(110, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label3.Location = new System.Drawing.Point(112, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label5.Location = new System.Drawing.Point(110, 594);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(223, 32);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Confirm Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label6.Location = new System.Drawing.Point(112, 512);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 32);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label7.Location = new System.Drawing.Point(112, 437);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 32);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Username";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label8.Location = new System.Drawing.Point(112, 363);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(87, 32);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Phone";
+            // 
+            // textname
+            // 
+            this.textname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textname.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textname.Location = new System.Drawing.Point(421, 204);
+            this.textname.Multiline = true;
+            this.textname.Name = "textname";
+            this.textname.Size = new System.Drawing.Size(216, 28);
+            this.textname.TabIndex = 10;
+            // 
+            // textpass
+            // 
+            this.textpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textpass.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textpass.Location = new System.Drawing.Point(421, 509);
+            this.textpass.Multiline = true;
+            this.textpass.Name = "textpass";
+            this.textpass.Size = new System.Drawing.Size(216, 28);
+            this.textpass.TabIndex = 11;
+            // 
+            // textuname
+            // 
+            this.textuname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textuname.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textuname.Location = new System.Drawing.Point(421, 434);
+            this.textuname.Multiline = true;
+            this.textuname.Name = "textuname";
+            this.textuname.Size = new System.Drawing.Size(216, 28);
+            this.textuname.TabIndex = 12;
+            // 
+            // textphn
+            // 
+            this.textphn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textphn.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textphn.Location = new System.Drawing.Point(421, 360);
+            this.textphn.Multiline = true;
+            this.textphn.Name = "textphn";
+            this.textphn.Size = new System.Drawing.Size(216, 28);
+            this.textphn.TabIndex = 13;
+            // 
+            // textadd
+            // 
+            this.textadd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textadd.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textadd.Location = new System.Drawing.Point(421, 267);
+            this.textadd.Multiline = true;
+            this.textadd.Name = "textadd";
+            this.textadd.Size = new System.Drawing.Size(300, 50);
+            this.textadd.TabIndex = 14;
+            // 
+            // textconpass
+            // 
+            this.textconpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textconpass.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textconpass.Location = new System.Drawing.Point(421, 591);
+            this.textconpass.Multiline = true;
+            this.textconpass.Name = "textconpass";
+            this.textconpass.Size = new System.Drawing.Size(216, 28);
+            this.textconpass.TabIndex = 15;
+            // 
+            // chkboxShowPass
+            // 
+            this.chkboxShowPass.AutoSize = true;
+            this.chkboxShowPass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkboxShowPass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkboxShowPass.Location = new System.Drawing.Point(646, 657);
+            this.chkboxShowPass.Name = "chkboxShowPass";
+            this.chkboxShowPass.Size = new System.Drawing.Size(151, 27);
+            this.chkboxShowPass.TabIndex = 16;
+            this.chkboxShowPass.Text = "Show Password";
+            this.chkboxShowPass.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(364, 722);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button2.Location = new System.Drawing.Point(364, 800);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 35);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(369, 887);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 23);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Already Have an Account?";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label9.Location = new System.Drawing.Point(417, 938);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 23);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Go to Login";
+            // 
+            // name
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(940, 1102);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkboxShowPass);
+            this.Controls.Add(this.textconpass);
+            this.Controls.Add(this.textadd);
+            this.Controls.Add(this.textphn);
+            this.Controls.Add(this.textuname);
+            this.Controls.Add(this.textpass);
+            this.Controls.Add(this.textname);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "name";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textname;
+        private System.Windows.Forms.TextBox textpass;
+        private System.Windows.Forms.TextBox textuname;
+        private System.Windows.Forms.TextBox textphn;
+        private System.Windows.Forms.TextBox textadd;
+        private System.Windows.Forms.TextBox textconpass;
+        private System.Windows.Forms.CheckBox chkboxShowPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ registrationadms/registrationadms/login.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace registrationadms
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chkboxShowPass = new System.Windows.Forms.CheckBox();
+            this.textuname = new System.Windows.Forms.TextBox();
+            this.textpass = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label9.Location = new System.Drawing.Point(345, 719);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(156, 23);
+            this.label9.TabIndex = 38;
+            this.label9.Text = "Create an Account";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(325, 663);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 23);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "Don\'t Have an Account?";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button2.Location = new System.Drawing.Point(320, 576);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 35);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(320, 498);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 35);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // chkboxShowPass
+            // 
+            this.chkboxShowPass.AutoSize = true;
+            this.chkboxShowPass.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkboxShowPass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkboxShowPass.Location = new System.Drawing.Point(584, 418);
+            this.chkboxShowPass.Name = "chkboxShowPass";
+            this.chkboxShowPass.Size = new System.Drawing.Size(151, 27);
+            this.chkboxShowPass.TabIndex = 34;
+            this.chkboxShowPass.Text = "Show Password";
+            this.chkboxShowPass.UseVisualStyleBackColor = true;
+            // 
+            // textuname
+            // 
+            this.textuname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textuname.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textuname.Location = new System.Drawing.Point(426, 229);
+            this.textuname.Multiline = true;
+            this.textuname.Name = "textuname";
+            this.textuname.Size = new System.Drawing.Size(216, 28);
+            this.textuname.TabIndex = 30;
+            // 
+            // textpass
+            // 
+            this.textpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.textpass.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textpass.Location = new System.Drawing.Point(430, 323);
+            this.textpass.Multiline = true;
+            this.textpass.Name = "textpass";
+            this.textpass.Size = new System.Drawing.Size(216, 28);
+            this.textpass.TabIndex = 29;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label7.Location = new System.Drawing.Point(165, 225);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 32);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Username";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label6.Location = new System.Drawing.Point(165, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 32);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(86)))), ((int)(((byte)(174)))));
+            this.label1.Location = new System.Drawing.Point(341, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 46);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Login";
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(819, 881);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkboxShowPass);
+            this.Controls.Add(this.textuname);
+            this.Controls.Add(this.textpass);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chkboxShowPass;
+        private System.Windows.Forms.TextBox textuname;
+        private System.Windows.Forms.TextBox textpass;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
